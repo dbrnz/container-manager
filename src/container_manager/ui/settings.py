bd5fcb77d71b7047689ce0fbe3c020820b726c6f
David Brooks <dbrnz@users.noreply.github.com>
2026-07-22 22:00:37 +1200

    Update `settings` dialog to use the native directory selector to set the root directory.

--- src/container_manager/ui/settings.py
+++ src/container_manager/ui/settings.py
@@ -10,15 +10,11 @@ import ttkbootstrap.constants as constants
 #===============================================================================
 
 from ..settings import Settings
+from .styles import LEFT_LINK_BUTTON_LABEL
 
 #===============================================================================
 
-def select_root_directory(root_directory: str|None=None) -> str|None:
-    root_directory = ttk.Querybox.get_directory(
-        title="Select the base directory that holds Workspaces",
-        initialdir=root_directory if root_directory is not None else Path.home()
-    )
-    return root_directory
+EMPTY_DIRECTORY = 'Click to set'
 
 #===============================================================================
 
@@ -26,30 +22,47 @@ class SettingsDialog(ttk.Dialog):
     def __init__(self, settings: Settings):
         super().__init__(parent=None, title='Container settings', alert=False)
         self.__settings = settings
+        self.__root_directory = ttk.StringVar(value=settings.root_directory if settings.root_directory is not None else EMPTY_DIRECTORY)
+        self.__port = ttk.StringVar(value=settings.port)
 
-    def create_body(self, master):
-        frame = ttk.Frame(master, padding=(20, 20))
-        self.root_directory = ttk.StringVar(value=self.__settings.root_directory)
-        self.port = ttk.IntVar(value=self.__settings.port)
-        self.create_form_field(frame, "root directory", self.root_directory)
-        port_field = self.create_form_field(frame, "port", self.port)
+    def create_body(self, parent):
+        frame = ttk.Frame(parent, padding=(20, 20))
+        self.__root_link = self.__create_entry_button(frame, "root directory", self.__root_directory)
+        port_field = self.__create_entry_field(frame, "port", self.__port)
+        ttk.Validation.numeric(port_field, when='all')
         ttk.Validation.range(port_field, 1024, 65535)
         frame.pack(fill=constants.X, expand=True)
 
-    def create_form_field(self, master, label: str, variable: ttk.Variable):
-        container = ttk.Frame(master)
+    def __create_entry_field(self, parent, label: str, variable: ttk.StringVar):
+        container = ttk.Frame(parent)
         container.pack(fill=constants.X, expand=constants.YES, pady=5)
-        lbl = ttk.Label(master=container, text=label.title(), width=10)
+        lbl = ttk.Label(container, text=f'{label.title()}:', width=10)
         lbl.pack(side=constants.LEFT, padx=5)
-        field = ttk.Entry(master=container, textvariable=variable)
-        field.pack(side=constants.LEFT, padx=5, fill=constants.X, expand=constants.YES)
+        field = ttk.Entry(container, textvariable=variable, width=5)
+        field.pack(side=constants.LEFT, padx=5, fill=constants.X, expand=constants.NO)
         return field
 
-    def create_buttonbox(self, master):
-        frame = ttk.Frame(master, padding=(5, 10))
+    def __create_entry_button(self, parent, label: str, variable: ttk.StringVar):
+        container = ttk.Frame(parent)
+        container.pack(fill=constants.X, expand=constants.YES, pady=5)
+        lbl = ttk.Label(container, text=f'{label.title()}:', width=10)
+        lbl.pack(side=constants.LEFT, padx=5)
+        button = ttk.Button(
+            container,
+            text=variable.get(),
+            bootstyle='link',
+            style=LEFT_LINK_BUTTON_LABEL,
+            width=40,
+            command=self.__set_directory
+        )
+        button.pack(side=constants.LEFT, padx=5, fill=constants.X, expand=constants.YES)
+        return button
+
+    def create_buttonbox(self, parent):
+        frame = ttk.Frame(parent, padding=(5, 10))
         sub_btn = ttk.Button(
             frame,
-            text="Submit",
+            text="Save",
             command=self.on_submit,
             bootstyle=constants.SUCCESS,
             width=6,
@@ -68,12 +81,24 @@ class SettingsDialog(ttk.Dialog):
 
     def on_submit(self):
         self._result = {
-            'root_directory': self.root_directory.get(),
-            'port': self.port.get(),
+            'root_directory': self.__root_directory.get(),
+            'port': self.__port.get(),
         }
         self.close()
 
     def on_cancel(self):
         self.close()
+
+    def __set_directory(self):
+        current_directory = self.__root_directory.get()
+        if current_directory == EMPTY_DIRECTORY:
+            current_directory = Path.home()
+        root_directory = ttk.Querybox.get_directory(
+            title="Select the base directory that holds Workspaces",
+            initialdir=current_directory
+        )
+        if root_directory is not None:
+            self.__root_link.configure(text=root_directory)
+            self.__root_directory.set(root_directory)
 
 #===============================================================================
